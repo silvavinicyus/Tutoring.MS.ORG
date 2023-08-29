40387dd88c0d1be07332a52f56463557e4b1d99e
Vinicyus Silva <vinicyus346@gmail.com>
2023-08-28 22:06:44 -0300

    feat: user entity

---
+++ src/1-domain/entities/user.ts
@@ -0,0 +1,44 @@
+import { AbstractEntity } from '@domain/abstractEntity'
+import { ITimestamps } from '@domain/timestamps'
+import { right, Right } from '@shared/either'
+
+export interface IUserEntity extends ITimestamps {
+  id: number
+  uuid: string
+  name: string
+  email: string
+  birthdate: Date
+  phone: string
+}
+
+export type IInputUserEntity = Pick<
+  IUserEntity,
+  'name' | 'email' | 'birthdate' | 'phone'
+>
+
+export class UserEntity extends AbstractEntity<IUserEntity> {
+  static create(
+    props: IInputUserEntity,
+    currentDate: Date
+  ): Right<void, UserEntity> {
+    const userEntity = new UserEntity({
+      id: undefined,
+      uuid: undefined,
+      created_at: currentDate,
+      updated_at: currentDate,
+      ...props,
+    })
+    return right(userEntity)
+  }
+
+  static update(
+    props: Partial<IUserEntity>,
+    currentDate: Date
+  ): Right<void, UserEntity> {
+    const userEntity = new UserEntity({
+      ...props,
+      updated_at: currentDate,
+    } as IUserEntity)
+    return right(userEntity)
+  }
+}
