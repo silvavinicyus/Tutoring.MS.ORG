3e80c143f2f34ef68b079c04e4632556894dbee7
Vinicyus Silva <vinicyus346@gmail.com>
2023-08-28 22:08:27 -0300

    feat: user migration

---
+++ src/4-framework/migrations/20230829005212-users-table.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const { DataTypes } = require('sequelize')
+
+module.exports = {
+  async up(
+    /** @type {import('sequelize').QueryInterface} */ queryInterface,
+    _Sequelize
+  ) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      uuid: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      phone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      birthdate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    })
+  },
+
+  async down(
+    /** @type {import('sequelize').QueryInterface} */ queryInterface,
+    _Sequelize
+  ) {
+    await queryInterface.dropTable('users')
+  },
+}
