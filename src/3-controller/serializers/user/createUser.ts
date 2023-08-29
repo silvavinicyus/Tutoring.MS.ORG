2bb10ab668df19337827c30b15e8970268098c3f
Vinicyus Silva <vinicyus346@gmail.com>
2023-08-28 22:07:48 -0300

    feat: user serializers

---
+++ src/3-controller/serializers/user/createUser.ts
@@ -0,0 +1,21 @@
+import { IsDate, IsNotEmpty, IsString } from 'class-validator'
+import { IInputCreateUserDto } from '@business/dto/user/createUserDto'
+import { AbstractSerializer } from '../abstractSerializer'
+
+export class InputCreateUser extends AbstractSerializer<IInputCreateUserDto> {
+  @IsString()
+  @IsNotEmpty()
+  name: string
+
+  @IsString()
+  @IsNotEmpty()
+  email: string
+
+  @IsDate()
+  @IsNotEmpty()
+  birthdate: Date
+
+  @IsString()
+  @IsNotEmpty()
+  phone: string
+}
