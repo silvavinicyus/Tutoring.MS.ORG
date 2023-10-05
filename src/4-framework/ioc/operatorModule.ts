1399e8959928b541f023916110ca2efe5c0936c9
Vinicyus Silva <vinicyus346@gmail.com>
2023-10-05 15:24:07 -0300

    feat: removing wrong operator import

--- src/4-framework/ioc/operatorModule.ts
+++ src/4-framework/ioc/operatorModule.ts
@@ -1,35 +1,34 @@
 import { ContainerModule, interfaces } from 'inversify'
-import { CreateUserOperator } from '@controller/operations/user/create'
-import { FindUserByOperator } from '@controller/operations/user/findBy'
-import { FindAllUsersOperator } from '@controller/operations/user/findAll'
-import { DeleteUserOperator } from '@controller/operations/user/delete'
-import { UpdateUserOperator } from '@controller/operations/user/update'
-import { CreateStudyGroupOperator } from '@controller/operations/studyGroup/create'
-import { FindAllStudyGroupsOperator } from '@controller/operations/studyGroup/findAll'
-import { FindByStudyGroupOperator } from '@controller/operations/studyGroup/findBy'
-import { DeleteStudyGroupOperator } from '@controller/operations/studyGroup/delete'
 import { CreatePostOperator } from '@controller/operations/post/create'
-import { FindPostByOperator } from '@controller/operations/post/findBy'
-import { FindAllPostsOperator } from '@controller/operations/post/findAll'
 import { DeletePostOperator } from '@controller/operations/post/delete'
+import { FindAllPostsOperator } from '@controller/operations/post/findAll'
+import { FindPostByOperator } from '@controller/operations/post/findBy'
 import { UpdatePostOperator } from '@controller/operations/post/update'
 import { CreatePostReactionOperator } from '@controller/operations/postReaction/create'
 import { DeletePostReactionOperator } from '@controller/operations/postReaction/delete'
 import { FindAllPostReactionsOperator } from '@controller/operations/postReaction/findAll'
+import { CreateStudyGroupOperator } from '@controller/operations/studyGroup/create'
+import { DeleteStudyGroupOperator } from '@controller/operations/studyGroup/delete'
+import { FindAllStudyGroupsOperator } from '@controller/operations/studyGroup/findAll'
+import { FindByStudyGroupOperator } from '@controller/operations/studyGroup/findBy'
 import { CreateStudyGroupLeaderOperator } from '@controller/operations/studyGroupLeader/create'
 import { DeleteStudyGroupLeaderOperator } from '@controller/operations/studyGroupLeader/delete'
 import { FindAllStudyGroupLeadersOperator } from '@controller/operations/studyGroupLeader/findAll'
+import { AnswerRequestToJoinStudyGroupOperator } from '@controller/operations/studyGroupStudent/answerRequestToJoin'
 import { CreateStudyGroupStudentOperator } from '@controller/operations/studyGroupStudent/create'
 import { DeleteStudyGroupStudentOperator } from '@controller/operations/studyGroupStudent/delete'
 import { FindAllStudyGroupStudentsOperator } from '@controller/operations/studyGroupStudent/findAll'
 import { RequestToJoinStudyGroupOperator } from '@controller/operations/studyGroupStudent/requestToJoin'
-import { AnswerRequestToJoinStudyGroupOperator } from '@controller/operations/studyGroupStudent/answerRequestToJoin'
 import { CreateTutoringOperator } from '@controller/operations/tutoring/create'
+import { DeleteTutoringOperator } from '@controller/operations/tutoring/delete'
 import { FindAllTutoringsOperator } from '@controller/operations/tutoring/findAll'
 import { FindTutoringByOperator } from '@controller/operations/tutoring/findBy'
 import { UpdateTutoringOperator } from '@controller/operations/tutoring/update'
-import { DeleteTutoringOperator } from '@controller/operations/tutoring/delete'
-import { UploadUserImageOperator } from '@controller/operations/user/uploadImage'
+import { CreateUserOperator } from '@controller/operations/user/create'
+import { DeleteUserOperator } from '@controller/operations/user/delete'
+import { FindAllUsersOperator } from '@controller/operations/user/findAll'
+import { FindUserByOperator } from '@controller/operations/user/findBy'
+import { UpdateUserOperator } from '@controller/operations/user/update'
 
 export const operatorModule = new ContainerModule((bind: interfaces.Bind) => {
   bind(CreateUserOperator).toSelf()
@@ -37,7 +36,6 @@ export const operatorModule = new ContainerModule((bind: interfaces.Bind) => {
   bind(FindAllUsersOperator).toSelf()
   bind(DeleteUserOperator).toSelf()
   bind(UpdateUserOperator).toSelf()
-  bind(UploadUserImageOperator).toSelf()
 
   bind(CreateStudyGroupOperator).toSelf()
   bind(FindAllStudyGroupsOperator).toSelf()
