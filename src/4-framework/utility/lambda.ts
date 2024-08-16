49b55127340fb0a9b02f42708cd19186c3b9e4a6
Vinicyus Silva <vinicyus346@gmail.com>
2024-08-16 08:21:51 -0300

    fix: connection sequelize error

--- src/4-framework/utility/lambda.ts
+++ src/4-framework/utility/lambda.ts
@@ -1,17 +1,20 @@
 import { RequestEventMiddyMiddleware } from '@framework/middlewares/eventRequestMethods'
+import { sequelize } from '@framework/utility/database'
 import middy from '@middy/core'
 import httpCors from '@middy/http-cors'
 import httpHeaderNormalizer from '@middy/http-header-normalizer'
 import jsonBodyParser from '@middy/http-json-body-parser'
 import httpMultipartBodyParser from '@middy/http-multipart-body-parser'
 import { Context } from 'aws-lambda'
-import { sequelize } from '@framework/utility/database'
 
 export const middyfy = (
   handler: (e: unknown, context?: Context) => Promise<unknown>
 ): middy.Middy<unknown, unknown, Context> =>
   middy(async (e: unknown, c: Context) => {
     sequelize.connectionManager.initPools()
+    if (sequelize.connectionManager.hasOwnProperty('getConnection')) {
+      delete sequelize.connectionManager.getConnection
+    }
     // eslint-disable-next-line no-param-reassign
     c.callbackWaitsForEmptyEventLoop = false
 
@@ -22,3 +25,7 @@ export const middyfy = (
     .use(httpMultipartBodyParser())
     .use(httpCors())
     .use(RequestEventMiddyMiddleware())
+    .after(async () => {
+      await sequelize.connectionManager.pool.drain()
+      return sequelize.connectionManager.pool.destroyAllNow()
+    })
